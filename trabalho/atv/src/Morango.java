dfc79cd467116eec2add6e777bb4eb85638ca840
O Pedro Rodrigues <98363013+prod01@users.noreply.github.com>
2024-10-11 17:06:37 -0300

    Add files via upload

---
+++ trabalho/atv/src/Morango.java
@@ -0,0 +1,83 @@
+public class Morango extends BaseDecorator {
+    private Tamanho tamanho;
+
+    public Morango(Adicional wrappee, Tamanho tamanho) {
+        super(wrappee);
+        this.tamanho = tamanho;
+    }
+
+    @Override
+    public String getDescricao() {
+        return wrappee.getDescricao() + ", Morango";
+    }
+
+    @Override
+    public double custo() {
+        switch (tamanho) {
+            case PEQUENO:
+                return wrappee.custo() + 1.00;
+            case MEDIO:
+                return wrappee.custo() + 1.50;
+            case GRANDE:
+                return wrappee.custo() + 2.00;
+            default:
+                return wrappee.custo();
+        }
+    }
+}
+
+public class Banana extends BaseDecorator {
+    private Tamanho tamanho;
+
+    public Banana(Adicional wrappee, Tamanho tamanho) {
+        super(wrappee);
+        this.tamanho = tamanho;
+    }
+
+    @Override
+    public String getDescricao() {
+        return wrappee.getDescricao() + ", Banana";
+    }
+
+    @Override
+    public double custo() {
+        switch (tamanho) {
+            case PEQUENO:
+                return wrappee.custo() + 1.00;
+            case MEDIO:
+                return wrappee.custo() + 1.50;
+            case GRANDE:
+                return wrappee.custo() + 2.00;
+            default:
+                return wrappee.custo();
+        }
+    }
+}
+
+public class LeiteCondensado extends BaseDecorator {
+    private Tamanho tamanho;
+
+    public LeiteCondensado(Adicional wrappee, Tamanho tamanho) {
+        super(wrappee);
+        this.tamanho = tamanho;
+    }
+
+    @Override
+    public String getDescricao() {
+        return wrappee.getDescricao() + ", Leite Condensado";
+    }
+
+    @Override
+    public double custo() {
+        switch (tamanho) {
+            case PEQUENO:
+                return wrappee.custo() + 1.00;
+            case MEDIO:
+                return wrappee.custo() + 1.50;
+            case GRANDE:
+                return wrappee.custo() + 2.00;
+            default:
+                return wrappee.custo();
+        }
+    }
+}
